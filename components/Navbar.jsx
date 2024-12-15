8abd7466519af70fe63214597157b2069111d7f2
mohdsaadshaikh <mohammadsaad925s4s@gmail.com>
2024-12-15 21:38:19 +0500

    notification model added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,11 +10,12 @@ import {
 import { ToggleTheme } from "@/components/ui/toggle-theme";
 import { useCurrentUser } from "@/hooks/useCurrentUse";
 import { logout } from "@/lib/actions/auth.action";
-import { EditIcon, User } from "lucide-react";
+import { EditIcon, LogOutIcon, User } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
 import { toast } from "sonner";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 export const Navbar = () => {
   const user = useCurrentUser();
   const router = useRouter();
@@ -29,6 +30,43 @@ export const Navbar = () => {
       <Logo />
       <div className="flex gap-6 items-center">
         <ToggleTheme />
+        {/* <Popover>
+          <PopoverTrigger asChild>
+            <Avatar size="sm" fallback={<AvatarFallback />}>
+              <AvatarImage src={user?.image || null} alt="User avatar" />
+              <AvatarFallback className="bg-[#53ab8b]">
+                <FaUser className="text-white" />
+              </AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent className="border border-black dark:border-white shadow-md">
+            <div className="flex gap-4 flex-col">
+              <div className="flex flex-col gap-2 items-center">
+                <Avatar size="sm" fallback={<AvatarFallback />}>
+                  <AvatarImage src={user?.image || null} alt="User avatar" />
+                  <AvatarFallback className="bg-[#53ab8b]">
+                    <FaUser className="text-white" />
+                  </AvatarFallback>
+                </Avatar>
+                <p className="text-lg font-semibold">{user?.name}</p>
+              </div>
+              <div className="flex justify-between">
+                <Link href="/profile/edit">
+                  <span className="cursor-pointer text-sm py-2 px-4 rounded-full underline">
+                    Edit Profile
+                  </span>
+                </Link>
+                <span
+                  className="cursor-pointer flex gap-3"
+                  onClick={handleLogout}
+                >
+                  <LogOutIcon />
+                  Logout
+                </span>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover> */}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar size="sm" fallback={<AvatarFallback />}>
@@ -56,7 +94,7 @@ export const Navbar = () => {
             </DropdownMenuItem>
             <DropdownMenuItem onClick={handleLogout}>
               <span className="cursor-pointer flex gap-3">
-                <FaSignOutAlt />
+                <LogOutIcon />
                 Logout
               </span>
             </DropdownMenuItem>
